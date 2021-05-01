cebfc975585f04c88be9050b4ed8d69fcb1d6dc6
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-05-01 23:57:21 +0000

    Added reflection classes for found annotations

---
+++ src/Locate/Parameter.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * PHP version 7.2 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Biurad\Annotations\Locate;
+
+class Parameter extends Annotation
+{
+    public function __construct(iterable $annotations, \ReflectionParameter $reflection)
+    {
+        $this->reflection = $reflection;
+        parent::__construct($annotations);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReflection(): \ReflectionParameter
+    {
+        return $this->reflection;
+    }
+}
